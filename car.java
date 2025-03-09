01edcdb99dc682f2a89fdfc579031d61be2223de
benamalinea <walterguzman174707@gmail.com>
2025-03-09 19:21:22 +0300

    car class

---
+++ car.java
@@ -0,0 +1,13 @@
+public class car {
+    String color;
+    String model;
+    String  make;
+    void start () {
+        System.out.println("The car has started");     
+    }
+     void accelerate () {
+        System.out.println("the car  has accelerated");
+
+    }
+
+}
